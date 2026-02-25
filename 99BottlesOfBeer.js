f784450b0a04123bebb67827015753101c06e0e8
Orson Stawiarski <orson@stazca.com>
2026-02-25 11:50:43 +0700

    I added console.log as previously undefined was being returned. Trying to do a return with multiple lines in my mind won't work, so I'm trying console.log.

---
+++ 99BottlesOfBeer.js
@@ -0,0 +1,14 @@
+var bottles = 99
+
+function bottlesOfBeer() {
+    while (bottles > 0) {
+        console.log(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer.");
+        bottles--;
+        console.log("Take one down and pass it around, " + bottles + " bottles of beer on the wall.");
+    
+    }
+    console.log("No more bottles of beer on the wall, no more bottles of beer.")
+    console.log("Go to the store and buy some more, 99 bottles of beer on the wall.")
+}
+
+bottlesOfBeer();
