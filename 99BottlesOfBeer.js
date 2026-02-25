7ce2916df3b4122398cdf3a9c66fb1595a775f3c
Orson Stawiarski <orson@stazca.com>
2026-02-25 12:06:42 +0700

    Adding else and if conditionals to make it more grammatically correct so that once we get down to one bottle of beer on the roll, it says <o´ne more bottle> rather than <one bottles of beer.>

--- 99BottlesOfBeer.js
+++ 99BottlesOfBeer.js
@@ -1,12 +1,15 @@
 var bottles = 99
 
 function bottlesOfBeer() {
-    while (bottles > 0) {
+    while (bottles >= 2) {
         console.log(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer.");
         bottles--;
-        console.log("Take one down and pass it around, " + bottles + " bottles of beer on the wall.");
-    
+        if (bottles === 1) console.log("Take one down and pass it around, " + bottles + " more bottles of beer on the wall.")
+        else {console.log("Take one down and pass it around, " + bottles + " bottles of beer on the wall.");}
     }
+    console.log(bottles + " more bottle of beer on the wall, " + bottles + " more bottle of beer.");
+    bottles--;
+    console.log("Take one down and pass it around, " + "no more bottles of beer on the wall.");
     console.log("No more bottles of beer on the wall, no more bottles of beer.")
     console.log("Go to the store and buy some more, 99 bottles of beer on the wall.")
 }
